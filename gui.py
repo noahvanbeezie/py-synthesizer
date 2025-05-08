cfefc53fbd2da4bb4ceb03fd7ffa434fc41454db
DogSniffers <DogSniffers@users.noreply.github.com>
2025-05-08 15:34:43 -0600

    Light code touch ups

--- gui.py
+++ gui.py
@@ -6,25 +6,30 @@ class GUI(tkinter.Tk):
         #Create base window
         super().__init__()
         self.title('Synth Example')
-        self.geometry('400x300')
+        self.geometry('800x600')
+
+        #This will be for setting the color, not sure on what color to use yet
+        #self.configure(bg="")
 
         #Setting base values
         self.padx = 10
         self.pady = 2
-        self.starting_value = 50 
+        self.slider_starting_value = 50 
+        self.slider_entry_width = 5
 
+        
         #Creating sliders
-        self.create_slider("volume", 0)
-        self.create_slider("example", 1)
+        self.create_slider("Volume", 0)
+        self.create_slider("Example", 1)
 
 
-    def create_slider(self, slider_name_, curr_row):
+    def create_slider(self, slider_name, curr_row):
         #Creating initial variables
         self.slider_val = tkinter.IntVar()
-        self.slider_title = tkinter.Label(text="Example Slider")
+        self.slider_title = tkinter.Label(text=slider_name)
         self.slider = tkinter.Scale(from_=0, to=100, orient=HORIZONTAL, showvalue=0, tickinterval=0, variable=self.slider_val)
-        self.slider.set(self.starting_value)
-        self.slider_input = tkinter.Entry(textvariable=self.slider_val)
+        self.slider.set(self.slider_starting_value)
+        self.slider_input = tkinter.Entry(textvariable=self.slider_val, width=self.slider_entry_width)
 
         #Setting up the display grid
         self.slider_title.grid(row=curr_row, column=0, padx=self.padx, pady=self.pady)
--- main.py
+++ main.py
@@ -2,16 +2,11 @@ import numpy as np
 import sounddevice as sd
 from gui import GUI
 
-import tkinter
-
 def play_sine(frequency, duration, rate=44100):
     t = np.linspace(0, duration, int(duration * rate), False)
     amplitude = np.sin(frequency * 2 * np.pi * t)
     sd.play(amplitude, samplerate=rate)
     sd.wait()
-
-def slider_test(value):
-    print("New value: ", value)
 
 def main():
     application = GUI()
